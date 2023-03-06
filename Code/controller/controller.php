a5eeda7d5b1a299f330df81bc4916876dddd84b2
Axwells <Axel.PITTET@cpnv.ch>
2023-03-06 15:04:12 +0100

    WIP

---
+++ Code/controller/controller.php
@@ -0,0 +1,7 @@
+<?php
+
+// Fonction qui permet d'afficher la page "home"
+function home()
+{
+    require "view/home.php";
+}
---
+++ Code/index.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+require "controller/controller.php";
+
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+    switch ($action) {
+        case 'home' :
+            home();
+            break;
+        default :
+            home();
+    }
+} else {
+    home();
+}
