72fd2b3cd34737809065a3a03f38d31e9fec0de2
Axwells <Axel.PITTET@cpnv.ch>
2023-03-16 14:57:42 +0100

    WIP

--- Code/view/home.php
+++ Code/view/home.php
@@ -15,7 +15,43 @@ ob_start();
             </div>
         </div>
     </div>
+    <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content text-center">
+            <div class="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure><img src="view/img/Search.png" alt="Shoes"/></figure>
+                <div class="card-body">
+                    <h2 class="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div class="divider-horizontal"></div>
+            <div class="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure><img src="view/img/Search.png" alt="Shoes"/></figure>
+                <div class="card-body">
+                    <h2 class="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+            <div class="divider-horizontal"></div>
+            <div class="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure><img src="view/img/Search.png" alt="Shoes"/></figure>
+                <div class="card-body">
+                    <h2 class="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+    </div>
 
 <?php
 $content = ob_get_clean();
